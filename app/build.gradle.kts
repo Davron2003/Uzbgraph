ef36a633e07695abb5ea88751db1b569186cf496
Davron2003 <89669247+Davron2003@users.noreply.github.com>
2026-06-24 17:02:43 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     // Telegram TDLib (vanyislands wrapper)
-    implementation("org.drinkless.tdlib:tdlib-android:1.8.0")
+    implementation("io.github.the-best-is-best:telegram-tdlib-android:1.1.0")
 }
